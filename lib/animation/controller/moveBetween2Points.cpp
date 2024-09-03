c7682279511167692606709133548c41e5d15a9b
Utsav Lal <ualal@ncsu.edu>
2024-09-03 13:25:59 -0400

    adding ability for platform to stop for some time

--- lib/animation/controller/moveBetween2Points.cpp
+++ lib/animation/controller/moveBetween2Points.cpp
@@ -6,23 +6,41 @@
 
 #include <SDL_timer.h>
 
-MoveBetween2Points::MoveBetween2Points(float point1, float point2, bool movingLeft): left(point1),
-    right(point2), movingLeft(movingLeft) {
+MoveBetween2Points::MoveBetween2Points(float point1, float point2, MovementState moveState, int waitTime): left(point1),
+    right(point2), state(moveState), waitTime(waitTime) {
+    currTime = SDL_GetTicks();
 }
 
 void MoveBetween2Points::moveBetween2Points(Object &object) {
     float currentX = object.rect.x;
-    if (movingLeft) {
-        if (currentX <= left) {
-            movingLeft = false;
-            return;
-        }
-        object.velocity.x = -100;
-    } else {
-        if (currentX >= right) {
-            movingLeft = true;
-            return;
-        }
-        object.velocity.x = 100;
+    switch (state) {
+        case LEFT:
+            if (currentX <= left) {
+                state = STOP;
+                currTime = SDL_GetTicks();
+                object.velocity.x = 0;
+                return;
+            }
+            object.velocity.x = -200;
+            break;
+        case RIGHT:
+            if (currentX >= right) {
+                state = STOP;
+                currTime = SDL_GetTicks();
+                object.velocity.x = 0;
+                return;
+            }
+            object.velocity.x = 200;
+            break;
+        case STOP:
+            Uint32 passedTime = SDL_GetTicks();
+            if ((passedTime - currTime) / 1000.f > waitTime) {
+                if (currentX <= left) {
+                    state = RIGHT;
+                } else {
+                    state = LEFT;
+                }
+            }
+            break;
     }
-};
+}
--- lib/animation/controller/moveBetween2Points.hpp
+++ lib/animation/controller/moveBetween2Points.hpp
@@ -6,16 +6,31 @@
 #define HORIZONTALMOVEMENTCONTROLLER_HPP
 #include "../../objects/object.hpp"
 
+enum MovementState {
+    LEFT,
+    RIGHT,
+    STOP
+};
+
+/**
+ * This class exposes a simple animation for a object to go from left to right and wait for few momements in between
+ * @param point1: x-coordinate of left corner
+ * @param point2: x-coordinate of right corner
+ * @param currState: the currState of the object, could be left or right only
+ * @param waitTime: integer value denoting the wait time for the object
+ */
 class MoveBetween2Points {
 public:
-    MoveBetween2Points(float point1, float point2, bool movingLeft);
+    MoveBetween2Points(float point1, float point2, MovementState currState, int waitTime);
 
     void moveBetween2Points(Object &object);
 
 private:
     float left;
     float right;
-    bool movingLeft;
+    MovementState state;
+    Uint32 currTime;
+    int waitTime;
 };
 
 #endif //HORIZONTALMOVEMENTCONTROLLER_HPP
