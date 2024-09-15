0d4651b8d3b1f3ee161e04cb0829f9147a870b7c
Utsav Lal <ualal@ncsu.edu>
2024-09-15 10:26:41 -0400

    Remove cmake-debug-folder

--- cmake-build-debug/CMakeFiles/shade_engine.dir/lib/game/GameManager.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/shade_engine.dir/lib/game/GameManager.cpp.o: \
-  /Users/jayeshgajbhar/CSC581_GameEngineFoundations/lib/game/GameManager.cpp \
-  /Users/jayeshgajbhar/CSC581_GameEngineFoundations/lib/game/GameManager.hpp \
-  /Users/jayeshgajbhar/CSC581_GameEngineFoundations/lib/game/../generic/singleton.hpp
--- cmake-build-debug/CMakeFiles/shade_engine.dir/lib/generic/singleton.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/shade_engine.dir/lib/generic/singleton.cpp.o: \
-  /Users/jayeshgajbhar/CSC581_GameEngineFoundations/lib/generic/singleton.cpp \
-  /Users/jayeshgajbhar/CSC581_GameEngineFoundations/lib/generic/singleton.hpp
